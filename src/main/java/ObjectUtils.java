ef14987010a0d5e9d52559e629f2a6033bd1559e
Geek05 <bharathakumar3@gmail.com>
2020-02-22 15:24:33 +0530

    Created new utility file

---
+++ src/main/java/ObjectUtils.java
@@ -0,0 +1,16 @@
+public class ObjectUtils {
+
+    public static <T> Object CloneObject(Object object, Class<T> className){
+        Gson gson = new Gson();
+        String jsonString = gson.toJson(object);
+        T clone = gson.fromJson(jsonString, className);
+        return clone;
+    }
+    
+    public static String GetSystemProperty(String propertyName, String defaultValue){
+        String propertyValue = System.getProperty(propertyName);
+        if(Objects.isNull(propertyValue))
+            propertyValue = System.getenv(propertyName);
+        return Objects.nonNull(propertyValue) ? propertyValue : defaultValue;
+    }
+}
